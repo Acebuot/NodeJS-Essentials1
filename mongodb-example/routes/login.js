fe1a1f9022a8136bbb2b55fca3d6cf93ec408a4f
unknown <L7Buot@gmail.com>
2021-05-20 12:13:04 -0400

    updated redirect and showing error messages

--- mongodb-example/routes/login.js
+++ mongodb-example/routes/login.js
@@ -5,51 +5,33 @@ const passport = require('passport');
 
 router.get('/login', (req, res, next) =>
 {
-    //loading without errors
-    const errors = [];
-    // outdated course method
-    // //array of errors or returns empty array
-    // const errors = req.flash().errors || [];
+    const errors = req.session.message || [];
+    if (req.session.message != undefined) req.session.message = undefined;
+    console.log(req.session.message);
     res.render('login', {messages: errors});
 });
 
-// outdated course method
-// //calling passport.authenticate first to see if it is correct (used Localstrategy in app.js)
-// //then it can continue to next funtion at 3rd param
-// router.post('/login', passport.authenticate('local',
-// {
-
-
-
-//     /// Which failureFlash does NOT work hahaha kms
-//     // //enable passport to flash messages back to route
-//     // //then redirect to login
-//     // failureFlash: true,
-//     // failureRedirect: '/login',
-    
-// }), (req,res,next) =>
-// {
-//     res.redirect('/secret');
-// });
-
 router.post('/login', function(req, res, next) 
 {
-    passport.authenticate('local', function(err, user, info) 
-    {
-      if(err) { return res.redirect('/info'); }
-      if(!user) { console.log(info);return res.render('login', {messages : info.message})}
-      req.logIn(user, function(err) {
-        if(err) { return next(err); }
-        return res.json(req.body.user);
-      })
-    })(req,res,next);
-  })
+  passport.authenticate('local', function(err, user, info) 
+  {
+    if(err) { return res.redirect('/info'); }
+    if(!user) { console.log(info);return res.render('login', {messages : info.message})}
+    req.logIn(user, function(err) {
+      if(err) { return next(err); }
+      return res.redirect('/secret');
+    })
+  })(req,res,next);
+})
 
 //reusable function redirecting unauthenticated request back to login
 const ensureAuthenticated = (req, res, next) => 
 {
     if (!req.isAuthenticated())
-        return res.redirect('/login');
+    {
+      req.session.message = 'You must first log in';
+      return res.redirect('/login');
+    }
     return next();
 }
 
